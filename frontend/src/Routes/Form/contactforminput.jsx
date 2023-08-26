cbab4bb4a3b611bfcf81ebffb93583b8b12e78b6
Abdallah DEKKICHE <119437598+abdellahd63@users.noreply.github.com>
2023-08-26 19:31:05 +0100

    today-push

--- frontend/src/Routes/Form/contactforminput.jsx
+++ frontend/src/Routes/Form/contactforminput.jsx
@@ -4,6 +4,7 @@ import './style/contactinput.css'
 const Contactforminput = (props) => {
   return (
     <div className='contactinput'>
+      //verification
         <label>{props.label}</label>
         <input placeholder={props.placeholder} type={props.type} width={props.width} />
     </div>
